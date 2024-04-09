7fe9b660ee3b680df6a41ed866ae5a69232e4e57
chantarawong <87411585+chantarawong@users.noreply.github.com>
2024-04-10 03:33:49 +0700

    initialization with copilot

---
+++ src/main/java/hello/spring/api/controller/HelloController.java
@@ -0,0 +1,18 @@
+package hello.spring.api.controller;
+
+import java.time.LocalDateTime;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+  @GetMapping("/hello")
+  public String HelloWorld(){
+
+    var localDateTime = LocalDateTime.now();
+    return "Hello World with Java Spring Boot - Maven at " + localDateTime.toString();
+  }
+
+
+}

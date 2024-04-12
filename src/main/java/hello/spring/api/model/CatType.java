b14dd9d931c4eb572544f536971a4b7ae9291efa
chantarawong <87411585+chantarawong@users.noreply.github.com>
2024-04-12 10:46:10 +0700

    Update CatType.java

--- src/main/java/hello/spring/api/model/CatType.java
+++ src/main/java/hello/spring/api/model/CatType.java
@@ -1,7 +1,35 @@
 package hello.spring.api.model;
 
 public enum CatType {
-  
 
+  // Enum constants
+  // Enum constants are static and final implicitly
+  // Enum constants are public
+  // please list me of cat type
+  // 1. Domestic
+  // 2. Wild
+  // 3. Stray
+  // 4. Feral
+  // 5. Hybrid
+  // 6. Farm
+  // 7. Community
+  // 8. Working
+  // 9. Show
+  // 10. Pet
+  // 11. Companion  
+  // 12. Barn
+  // 13. Street
+  // 14. Alley
+  // 15. Tame
+  // 16. Semi-feral
+  // 17. Semi-domestic
+  // 18. Semi-wild
+  // 19. Semi-tame
   
+  DOMESTIC,
+  WILD,
+  STRAY,
+  FERAL,
+  HYBRID
+
 }
